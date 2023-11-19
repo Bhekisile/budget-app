d3f7d8183db0389c425cc4c5b1fef137158330b0
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-19 20:09:41 +0200

    Install rspec and create tests for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :entities, class_name: 'entity', foreign_key: :author_id, dependent: :destroy
   has_many :groups, dependent: :destroy
-  
+
   validates :name, presence: true
 end
