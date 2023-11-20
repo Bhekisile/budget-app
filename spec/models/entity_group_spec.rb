f6468331e86841b460d7e21099583c056d36355c
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-20 05:58:32 +0200

    Add unit tests for models

---
+++ spec/models/entity_group_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'Entity group', type: :request do
+
+  it 'has many entity' do
+    association = EntityGroup.reflect_on_association(:entity)
+    expect(association.macro).to eq(:has_many)
+  end
+
+  it 'has many group' do
+    association = EntityGroup.reflect_on_association(:group)
+    expect(association.macro).to eq(:has_many)
+  end
+end
