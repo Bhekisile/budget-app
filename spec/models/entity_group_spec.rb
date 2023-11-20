36365faa6359d505c89fc14a0e4b9349e946ac30
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-20 06:05:11 +0200

    Fix linter errors

--- spec/models/entity_group_spec.rb
+++ spec/models/entity_group_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Entity group', type: :request do
-
   it 'has many entity' do
     association = EntityGroup.reflect_on_association(:entity)
     expect(association.macro).to eq(:has_many)
